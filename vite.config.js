145873c7ce5c3bf0feedc4a67dff7931547ed343
Amit Ayre <amit.ayre@dialpad.com>
2025-11-25 18:16:47 +0530

    Fix base URL for new GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/daart-onboarding-v3/',
+  base: '/daart-onboarding-v3-latest/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
